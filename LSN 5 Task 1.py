6495f0cacfe7b32f182727b791130d5976ab2c2e
zxyd_amr <mail.amrziyad@gmail.com>
2026-05-23 13:53:21 +0300

    Add files via upload

---
+++ LSN 5 Task 1.py
@@ -0,0 +1,16 @@
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+
+if num1 > 0:
+    print(num1, "is a positive number.")
+elif num1 < 0:
+    print(num1, "is a negative number.")
+else:
+    print(num1, "is zero.")
+
+if num2 > 0:
+    print(num2, "is a positive number.")
+elif num2 < 0:
+    print(num2, "is a negative number.")
+else:
+    print(num2, "is zero.")
